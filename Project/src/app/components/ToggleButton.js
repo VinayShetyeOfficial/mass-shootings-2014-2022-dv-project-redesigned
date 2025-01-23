e7e6feef15bf3518e89e67acebb9b0eb145e1140
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-22 22:29:58 -0500

    updated file

--- Project/src/app/components/ToggleButton.js
+++ Project/src/app/components/ToggleButton.js
@@ -5,7 +5,7 @@ export default function ToggleButton({ toggleVisibility }) {
   return (
     <button
       onClick={toggleVisibility}
-      className="absolute px-6 py-3 text-lg font-bold text-white transition-transform duration-500 bg-blue-600 rounded-lg shadow-lg bottom-2 left-2"
+      className="absolute px-6 py-3 text-lg font-bold text-white transition-all duration-300 rounded-lg bottom-2 left-2 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:outline-none"
     >
       Toggle Menu
     </button>
