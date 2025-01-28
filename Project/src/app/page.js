5aea5653d3df67ba434e611f99e0a8129f545714
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-27 19:39:02 -0500

    updated page

--- Project/src/app/page.js
+++ Project/src/app/page.js
@@ -1,14 +1,20 @@
+// src/app/page.js
+
 "use client";
-import { useState } from "react";
+
+import { useContext, useState, Suspense } from "react";
 import MapComponent from "./components/MapComponent";
 import ExploreMenu from "./components/ExploreMenu";
 import VisualizationMenu from "./components/VisualizationMenu";
 import YearSelector from "./components/YearSelector";
 import ResetZoomButton from "./components/ResetZoomButton";
 import ToggleButton from "./components/ToggleButton";
+import Loader from "./components/Loader"; // Correct import
+import { MapContext } from "./context/MapContext";
 
 export default function MainPage() {
   const [menuVisible, setMenuVisible] = useState(true);
+  const { isMapLoaded } = useContext(MapContext);
 
   const toggleVisibility = () => {
     setMenuVisible((prev) => !prev);
@@ -16,12 +22,17 @@ export default function MainPage() {
 
   return (
     <div className="relative">
+      <Suspense fallback={<div />}>{!isMapLoaded && <Loader />}</Suspense>
       <MapComponent />
-      <ExploreMenu isVisible={menuVisible} />
-      <VisualizationMenu isVisible={menuVisible} />
-      <YearSelector isVisible={menuVisible} />
-      <ResetZoomButton />
-      <ToggleButton toggleVisibility={toggleVisibility} />
+      {isMapLoaded && (
+        <>
+          <ExploreMenu isVisible={menuVisible} />
+          <VisualizationMenu isVisible={menuVisible} />
+          <YearSelector isVisible={menuVisible} />
+          <ResetZoomButton />
+          <ToggleButton toggleVisibility={toggleVisibility} />
+        </>
+      )}
     </div>
   );
 }
